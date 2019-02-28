2e6f5cf129f63d86fe4873f746ca2c0f51b8b5f5
Ricardo Calejo <ricardo.calejo@outlook.com>
2019-02-28 14:47:11 +0000

    Print img to new Page

--- PrintSharePoint/src/extensions/printPage/PrintPageApplicationCustomizer.ts
+++ PrintSharePoint/src/extensions/printPage/PrintPageApplicationCustomizer.ts
@@ -121,12 +121,15 @@ export default class PrintPageApplicationCustomizer
     var canvasToImg = function(canvas, loaded, error){
       var dataURL = canvas.toDataURL('image/png'),
         img = new Image();
-      img.onload = loaded;
-      img.onerror = error;
-      img.src = dataURL;
       if(!isPdf){
-        window.open(dataURL, '_blank');
-      };
+        var win = window.open();
+        win.document.write('<iframe src="' + dataURL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+      }
+      else{
+        img.onload = loaded;
+        img.onerror = error;
+        img.src = dataURL;
+      }
     };
     
     var imageToPdf = function(){
@@ -181,12 +184,7 @@ export default class PrintPageApplicationCustomizer
           }
           ).
           then((canvas) => {
-            if(!isPdf){
-              canvasToImg(canvas, null, null);
-            }else{
               canvasToImg(canvas, imageToPdf, imageLoadError);
-            }
-            
         });
         ($(".SPPageChrome") as any).ploading({
           action: 'hide'
